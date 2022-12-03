33d5f9f3d633284f9d7e07e376abd69a97f118fd
naxucrishvili1993 <giorginaxucrishvili70@gmail.com>
2022-12-03 23:54:47 +0400

    Customize category buttons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,62 +1,56 @@
 import React from "react";
 
 const Sidebar = (props) => {
+	const buttons = document.querySelectorAll(".category-btn");
+	const handleClick = (e) => {
+		buttons.forEach((el) => {
+			el.classList.remove("active-btn");
+		});
+		props.handleChange(e);
+		e.currentTarget.classList.toggle("active-btn");
+	};
+
 	return (
 		<div className="sidebar">
 			<ul className="category-list">
 				<li>
-					<button
-						className="category-btn"
-						onClick={props.handleChange}
-						value="All">
+					<button className="category-btn" onClick={handleClick} value="All">
 						All
 					</button>
 				</li>
 				<li>
 					<button
-						className="category-btn"
-						onClick={props.handleChange}
+						className={"category-btn"}
+						onClick={handleClick}
 						value="Shirts">
 						Shirts
 					</button>
 				</li>
 				<li>
-					<button
-						className="category-btn"
-						onClick={props.handleChange}
-						value="Jeans">
+					<button className="category-btn" onClick={handleClick} value="Jeans">
 						Jeans
 					</button>
 				</li>
 				<li>
 					<button
 						className="category-btn"
-						onClick={props.handleChange}
+						onClick={handleClick}
 						value="Jackets">
 						Jackets
 					</button>
 				</li>
 				<li>
-					<button
-						className="category-btn"
-						onClick={props.handleChange}
-						value="Pants">
+					<button className="category-btn" onClick={handleClick} value="Pants">
 						Pants
 					</button>
 				</li>
 				<li>
-					<button
-						className="category-btn"
-						onClick={props.handleChange}
-						value="Shoes">
+					<button className="category-btn" onClick={handleClick} value="Shoes">
 						Shoes
 					</button>
 				</li>
 				<li>
-					<button
-						className="category-btn"
-						onClick={props.handleChange}
-						value="Socks">
+					<button className="category-btn" onClick={handleClick} value="Socks">
 						Socks
 					</button>
 				</li>
